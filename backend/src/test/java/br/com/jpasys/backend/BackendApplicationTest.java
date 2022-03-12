0115b114bbe94f2e2880ad6603fb4b778f40afcd
calvarengapb <c.alvarenga.pb@hotmail.com>
2022-03-12 14:02:50 -0400

    Projeto Criado

---
+++ backend/src/test/java/br/com/jpasys/backend/BackendApplicationTest.java
@@ -0,0 +1,14 @@
+package br.com.jpasys.backend;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class BackendApplicationTest {
+
+	@Test
+	void testMain() {
+		fail("Not yet implemented");
+	}
+
+}
